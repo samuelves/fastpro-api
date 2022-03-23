c1ce4bca8a20aaeb22eb04985258a5a99344d511
Ives Samuel <ivessamuel64@gmail.com>
2022-03-22 22:18:13 -0300

    Docker

--- src/presentation/infrastructure/http/server.ts
+++ src/presentation/infrastructure/http/server.ts
@@ -2,6 +2,6 @@ import ora from 'ora';
 
 import { app } from './app';
 
-app.listen(process.env.PORT || 999, () => {
+app.listen(process.env.PORT || 3000, () => {
   return (ora('Server is running...').start().color = 'green');
 });
